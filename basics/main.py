26d2b521f41e8a4068dc966bda85291add03bda2
amuthanmannan <amuthan.m@thoughtworks.com>
2021-02-23 13:44:31 +0530

    Added more actions and transformations

--- basics/main.py
+++ basics/main.py
@@ -10,28 +10,58 @@ def getInputRdd():
 def filterWordsWithI():
     print("Fitering words with I ")
     inputRDD = getInputRdd()
-    iRDD = inputRDD.filter(lambda x: "i" in x)
-    l = iRDD.collect()
+    iRDD = inputRDD.filter(lambda x: "i" in x)  # This is called Transformations: Transformations are operations on RDDs that return a new RDD. e.g filter,union
+    l = iRDD.collect() # This is called Actions: Actions are the second type of RDD operation. They are the operations that return a final value to the driver program or write data to an external storage system.
     print(l)
 
 def filterWordsWithE():
     print("Fitering words with E ")
     inputRDD = getInputRdd()
-    eRDD = inputRDD.filter(lambda x: "e" in x)
-    l = eRDD.collect()
+    eRDD = inputRDD.filter(lambda x: "e" in x) # Transformations
+    l = eRDD.collect() # Actions
     print(l)
 
 def filterWordsWithIandE():
     inputRDD = getInputRdd()
-    iRDD = inputRDD.filter(lambda x: "i" in x)
-    eRDD = inputRDD.filter(lambda x: "e" in x)
-    ieRDD=eRDD.union(iRDD).distinct()
-    l = ieRDD.collect()
+    iRDD = inputRDD.filter(lambda x: "i" in x) # Transformations
+    eRDD = inputRDD.filter(lambda x: "e" in x) # Transformations
+    ieRDD=eRDD.union(iRDD).distinct() # Transformations
+    l = ieRDD.collect() # Actions
     print(l)
+
+def getFirst2Words():
+    inputRDD = getInputRdd()
+    twoWordList=inputRDD.take(2)  # Action:
+    print(twoWordList)
+
+def ChangeAllWordsToUppercase():
+    inputRDD = getInputRdd()
+    allWords=inputRDD.map(lambda word: # Transformations
+         word.upper()
+    ).collect()  # Action:
+    print(allWords)
+
+
+def CreatePairOfLowerAndUppercase():
+    inputRDD = getInputRdd()
+    allWordsRdd=inputRDD.map(lambda word: # Transformations
+                          (word,word.upper())
+    )
+    # Combined keys and values
+    print(allWordsRdd.collect())
+
+    # keys only - normal case
+    print(allWordsRdd.keys().collect())
+
+    # values only - upper case
+    print(allWordsRdd.values().collect())
 
 
 if __name__ == '__main__':
     filterWordsWithI()
     filterWordsWithE()
     filterWordsWithIandE()
+    getFirst2Words()
+    ChangeAllWordsToUppercase()
+    CreatePairOfLowerAndUppercase()
 
